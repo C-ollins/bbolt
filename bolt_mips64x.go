314f64ad44cfaab41fbcf5151104520b534b77dd
Collins Olanrewaju <ocollins444@gmail.com>
2021-03-29 19:14:06 +0100

    Set max heap size of all processors to 500mb

--- bolt_mips64x.go
+++ bolt_mips64x.go
@@ -3,7 +3,7 @@
 package bbolt
 
 // maxMapSize represents the largest mmap size supported by Bolt.
-const maxMapSize = 0x8000000000 // 512GB
+const maxMapSize = 0x1F400000 // 500MB
 
 // maxAllocSize is the size used when creating array pointers.
-const maxAllocSize = 0x7FFFFFFF
+const maxAllocSize = 0xFFFFFFF // 268MB
